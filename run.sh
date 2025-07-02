17da4bc00126867f79e025cc4b197e946d0e5fbb
DoganM95 <doganmermertas@hotmail.de>
2025-07-02 19:34:41 +0200

    update test commands

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-docker run --rm -it \
+docker run -d \
     --privileged \
-    --net=host \
+    -p 3000:3000 \
     --device /dev/bus/usb:/dev/bus/usb \
     -v /sys:/sys:ro \
+    openrgb-server:latest
+
+docker run -d `
+    --privileged `
+    -p 3000:3000 `
+    --device /dev/bus/usb:/dev/bus/usb `
+    -v /sys:/sys:ro `
     openrgb-server:latest
