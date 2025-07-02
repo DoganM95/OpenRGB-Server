06e3bb5f1cb31becac9c250dbf9b4d31340d4cfc
DoganM95 <doganmermertas@hotmail.de>
2025-07-02 19:55:06 +0200

    update commands

--- run.sh
+++ run.sh
@@ -11,3 +11,21 @@ docker run -d `
     --device /dev/bus/usb:/dev/bus/usb `
     -v /sys:/sys:ro `
     openrgb-server:latest
+
+
+
+docker run --rm -it \
+    -p 6742:6742 \
+    --privileged \
+    --name openrgb-tcp-server \
+    --device /dev/bus/usb:/dev/bus/usb \
+    -v /sys:/sys:ro \
+    openrgb-tcp-server:latest
+
+docker run --rm -it `
+    -p 6742:6742 `
+    --privileged `
+    --name openrgb-tcp-server `
+    --device /dev/bus/usb:/dev/bus/usb `
+    -v /sys:/sys:ro `
+    openrgb-tcp-server:latest
