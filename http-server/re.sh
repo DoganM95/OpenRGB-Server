07ac9b9551f8073115abdc04e18e001ae8381c18
DoganM95 <doganmermertas@hotmail.de>
2026-01-23 00:26:43 +0100

    create shell script for quick deployment & testing during development

--- http-server/re.sh
+++ http-server/re.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker build -t rgb .
 
 docker rm -f rgb
@@ -6,9 +8,8 @@ docker run -d \
   -e "OPENRGB_HOST=10.0.0.200" \
   -e "OPENRGB_PORT=6742" \
     --privileged \
-  --name doganm95-openrgb-http-server \
+  --name rgb \
     --device /dev/bus/usb:/dev/bus/usb \
-  --pull always \
   -p 6744:3000 \
   --restart always \
     -v /sys:/sys:ro \
