ea304262ca87c5d02e3f73fac492d12638cd8fde
DoganM95 <doganmermertas@hotmail.de>
2026-01-23 02:47:36 +0100

    fix permissions & rm redundancy from docker run command of client

--- http-server/re.sh
+++ http-server/re.sh
@@ -7,10 +7,7 @@ docker rm -f rgb
 docker run -d \
   -e "OPENRGB_HOST=10.0.0.200" \
   -e "OPENRGB_PORT=6742" \
-    --privileged \
   --name rgb \
-    --device /dev/bus/usb:/dev/bus/usb \
   -p 6744:3000 \
   --restart always \
-    -v /sys:/sys:ro \
   rgb
